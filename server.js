8fb321ff01c2320bf3aca530321fbf35ee1e6210
Rami2212 <ramithapathmilarp@gmail.com>
2025-11-09 07:41:43 +0530

    fix

--- server.js
+++ server.js
@@ -68,10 +68,10 @@ app.set('trust proxy', 1);
 app.use(rateLimitMiddleware.globalLimiter);
 
 // Analytics middleware
-//app.use(analyticsMiddleware);
+app.use(analyticsMiddleware);
 
 // SEO middleware
-//app.use(seoMiddleware());
+app.use(seoMiddleware());
 
 app.use(async (req, res, next) => {
   try {
